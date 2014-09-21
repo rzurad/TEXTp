cfb2fda08f216d66b074656c0e9bbedb5ac7fb41
Richard Zurad <rzurad@gmail.com>
2014-09-20 20:06:57 -0700

    add about page

--- app/routes/images.js
+++ app/routes/images.js
@@ -1,11 +1,17 @@
 import Ember from "ember";
 
-var ImagesRoute = Ember.Route.extend({
-        beforeModel: function () {
-            if (!this.controllerFor('images').get('content.length')) {
-                this.transitionTo('index');
-            }
+var ImagesRoute;
+
+ImagesRoute = Ember.Route.extend({
+    activate: function () {
+        Ember.$(document).attr('title', 'TEXTp - Images');
+    },
+
+    beforeModel: function () {
+        if (!this.controllerFor('images').get('content.length')) {
+            this.transitionTo('index');
         }
-    });
+    }
+});
 
 export default ImagesRoute;
---
+++ app/routes/index.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    activate: function () {
+        Ember.$(document).attr('title', 'TEXTp');
+    }
+});
