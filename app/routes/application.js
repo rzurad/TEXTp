ba746d6456e16b7e59cc14f6225a8862d9be2983
Richard Zurad <rzurad@gmail.com>
2014-09-22 17:53:32 -0700

    stub in feature detection

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,20 @@
 import Ember from 'ember';
 import { isMobileOrTablet } from '../utils/mobile-detect';
+import isCompatibleEnv from '../utils/feature-detect';
 
 var ApplicationRoute;
 
 ApplicationRoute = Ember.Route.extend({
     beforeModel: function () {
-        if (isMobileOrTablet()) {
+        if (isMobileOrTablet() || !isCompatibleEnv()) {
+            // redirect to unsupported page if it looks like the app won't work
+            // or is on a mobile browser
             this.transitionTo('unsupported');
+        } else {
+            // always redirect to index on app init. This is so you can't land on
+            // the images route when there are no images and also so that you don't
+            // get stuck on the unsupported page during development
+            this.transitionTo('index');
         }
     }
 });
--- app/routes/images.js
+++ app/routes/images.js
@@ -5,12 +5,6 @@ var ImagesRoute;
 ImagesRoute = Ember.Route.extend({
     activate: function () {
         Ember.$(document).attr('title', 'TEXTp - Images');
-    },
-
-    beforeModel: function () {
-        if (!this.controllerFor('images').get('content.length')) {
-            this.transitionTo('index');
-        }
     }
 });
 
